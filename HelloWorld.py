4ac3c3b8bcca3cd92cf66a2e936b3a18a8b5e8cf
Gcomune <comuneg@hotmail.com>
2022-08-22 17:54:02 -0400

    Modifications on the branch

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
 print("Hello World\n")
 
 #Just adding comments
+#These comments were added from the branch; file needs to be merged with Master
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
 Just a simple text file
+But just added some comments
+These are also new            
